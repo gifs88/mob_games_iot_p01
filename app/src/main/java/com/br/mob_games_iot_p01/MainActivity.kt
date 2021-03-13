0734863c071b68b757b185fd0699440f965cc7b0
gifs88 <rafamatosaraujo@gmail.com>
2021-03-13 12:44:22 -0300

    start project

---
+++ app/src/main/java/com/br/mob_games_iot_p01/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.br.mob_games_iot_p01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
