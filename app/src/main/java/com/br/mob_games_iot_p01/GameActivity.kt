1f190bba12a3d966813f102685a3102865f810f7
gifs88 <rafamatosaraujo@gmail.com>
2021-03-14 19:17:57 -0300

    definido tarefas do step 3

--- app/src/main/java/com/br/mob_games_iot_p01/GameActivity.kt
+++ app/src/main/java/com/br/mob_games_iot_p01/GameActivity.kt
@@ -7,5 +7,9 @@ class GameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+
+        //TODO 04: Link com botões da tela
+        //TODO 05: Adicionar listener a cada um dos botões
+        //TODO 06: Subir toaster com nome do botões ao serem clicados
     }
 }
