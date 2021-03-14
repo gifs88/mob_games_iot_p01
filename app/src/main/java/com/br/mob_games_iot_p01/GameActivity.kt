74d7cf0a518b6d565afaf702acb3c818cf8c74f0
gifs88 <rafamatosaraujo@gmail.com>
2021-03-14 19:38:56 -0300

    resolve step 03

--- app/src/main/java/com/br/mob_games_iot_p01/GameActivity.kt
+++ app/src/main/java/com/br/mob_games_iot_p01/GameActivity.kt
@@ -2,14 +2,38 @@ package com.br.mob_games_iot_p01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class GameActivity : AppCompatActivity() {
+
+    private lateinit var btnPedra: Button
+    private lateinit var btnPapel: Button
+    private lateinit var btnTesoura: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
 
-        //TODO 04: Link com botões da tela
-        //TODO 05: Adicionar listener a cada um dos botões
-        //TODO 06: Subir toaster com nome do botões ao serem clicados
+        setupViews()
+    }
+
+    private fun setupViews() {
+        btnPedra = findViewById(R.id.pedra)
+        btnPapel = findViewById(R.id.papel)
+        btnTesoura = findViewById(R.id.tesoura)
+        setupListeners()
+    }
+
+    private fun setupListeners() {
+        btnPedra.setOnClickListener {
+            Toast.makeText(this, getString(R.string.pedra), Toast.LENGTH_SHORT).show()
+        }
+        btnPapel.setOnClickListener {
+            Toast.makeText(this, getString(R.string.papel), Toast.LENGTH_SHORT).show()
+        }
+        btnTesoura.setOnClickListener {
+            Toast.makeText(this, getString(R.string.tesoura), Toast.LENGTH_SHORT).show()
+        }
     }
 }
