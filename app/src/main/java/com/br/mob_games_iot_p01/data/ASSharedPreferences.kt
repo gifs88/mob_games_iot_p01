3eb1af77733cd1eaedc28ee7f1be1d043a517b51
gifs88 <rafamatosaraujo@gmail.com>
2021-04-03 18:28:21 -0300

    shared preferences basic

---
+++ app/src/main/java/com/br/mob_games_iot_p01/data/ASSharedPreferences.kt
@@ -0,0 +1,36 @@
+package com.br.mob_games_iot_p01.data
+
+import android.content.Context
+import android.content.SharedPreferences
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+
+class ASSharedPreferences(private val context: Context) {
+
+    private val gson = Gson()
+    private val sharedPref: SharedPreferences
+    private val name = "game_prefs"
+    private val ranking = "ranking"
+
+    init {
+        sharedPref = context.getSharedPreferences(name, Context.MODE_PRIVATE)
+    }
+
+    fun updateRanking(item: RankingItem) {
+        val rankingList = calculateRankingPosition(getRanking())
+        val rankingString = gson.toJson(rankingList)
+
+        val editor = sharedPref.edit()
+        editor.putString(ranking, rankingString)
+        editor.apply()
+    }
+
+    fun getRanking(): ArrayList<RankingItem> {
+        val rankingString = sharedPref.getString(ranking, null) ?: return arrayListOf()
+        return gson.fromJson(rankingString, object : TypeToken<ArrayList<RankingItem>>() {}.type)
+    }
+
+    fun calculateRankingPosition(ranking: ArrayList<RankingItem>): ArrayList<RankingItem> {
+        return  ranking
+    }
+}
